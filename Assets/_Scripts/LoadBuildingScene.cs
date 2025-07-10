85b6c8b3724d37bd595add32aa1586b98b697d89
inkstallgames <inkstallsushant@gmail.com>
2025-07-10 17:11:22 +0530

    Added Enter Building Particle Effect

---
+++ Assets/_Scripts/LoadBuildingScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBuildingScene : MonoBehaviour
+{
+   private void OnTriggerEnter(Collider other)
+   {
+        if (other.tag == "Player")
+        {
+            Invoke("LoadNextScene", 1f);
+        }
+   }
+
+   private void LoadNextScene()
+   {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+}

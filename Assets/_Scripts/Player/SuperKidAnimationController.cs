e32e32e11eb2334b879e0c41b2948de216bc3ed9
inkstallgames <inkstallsushant@gmail.com>
2025-07-17 11:38:13 +0530

    Update

--- Assets/_Scripts/Player/SuperKidAnimationController.cs
+++ Assets/_Scripts/Player/SuperKidAnimationController.cs
@@ -22,6 +22,9 @@ public class SuperKidAnimationController : MonoBehaviour
     [SerializeField] private float runThreshold = 0.7f; // Joystick magnitude threshold for running
     [SerializeField] private float directionThreshold = 0.3f; // Threshold for determining direction
 
+    [Header("Debug")]
+    [SerializeField] private bool showDebugLogs = true;
+
     // References
     private Animator animator;
     private StarterAssetsInputs starterAssetsInputs;
@@ -76,6 +79,7 @@ public class SuperKidAnimationController : MonoBehaviour
         if (isJumping)
         {
             animator.SetBool(jumpingParam, true);
+            if (showDebugLogs) Debug.Log($"Animation State: Jumping");
             return;
         }
         
@@ -83,6 +87,7 @@ public class SuperKidAnimationController : MonoBehaviour
         if (!isMoving)
         {
             animator.SetBool(idleParam, true);
+            if (showDebugLogs) Debug.Log($"Animation State: Idle");
             return;
         }
         
@@ -95,6 +100,8 @@ public class SuperKidAnimationController : MonoBehaviour
         // Make sure angle is between 0 and 360
         if (angle < 0) angle += 360f;
         
+        if (showDebugLogs) Debug.Log($"Move Input: {moveInput}, Magnitude: {magnitude}, Angle: {angle}");
+        
         // Determine direction based on angle
         if (isRunning)
         {
@@ -102,18 +109,22 @@ public class SuperKidAnimationController : MonoBehaviour
             if (angle >= 315 || angle < 45) // Forward
             {
                 animator.SetBool(runningParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Running Forward");
             }
             else if (angle >= 45 && angle < 135) // Right
             {
                 animator.SetBool(runningRightForwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Running Right Forward");
             }
             else if (angle >= 225 && angle < 315) // Left
             {
                 animator.SetBool(runningLeftForwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Running Left Forward");
             }
             else // Backward - use walking backward since we don't have running backward
             {
                 animator.SetBool(walkingBackwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Backward (No Running Backward)");
             }
         }
         else
@@ -122,34 +133,42 @@ public class SuperKidAnimationController : MonoBehaviour
             if (angle >= 337.5 || angle < 22.5) // Forward
             {
                 animator.SetBool(walkingParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Forward");
             }
             else if (angle >= 22.5 && angle < 67.5) // Forward-Right
             {
                 animator.SetBool(walkingRightForwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Right Forward");
             }
             else if (angle >= 67.5 && angle < 112.5) // Right
             {
                 animator.SetBool(walkingRightParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Right");
             }
             else if (angle >= 112.5 && angle < 157.5) // Backward-Right
             {
                 animator.SetBool(walkingRightBackwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Right Backward");
             }
             else if (angle >= 157.5 && angle < 202.5) // Backward
             {
                 animator.SetBool(walkingBackwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Backward");
             }
             else if (angle >= 202.5 && angle < 247.5) // Backward-Left
             {
                 animator.SetBool(walkingLeftBackwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Left Backward");
             }
             else if (angle >= 247.5 && angle < 292.5) // Left
             {
                 animator.SetBool(walkingLeftParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Left");
             }
             else if (angle >= 292.5 && angle < 337.5) // Forward-Left
             {
                 animator.SetBool(walkingLeftForwardParam, true);
+                if (showDebugLogs) Debug.Log($"Animation State: Walking Left Forward");
             }
         }
     }

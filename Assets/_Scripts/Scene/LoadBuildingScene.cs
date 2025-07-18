c98d3d66b6fe98da9235b52a066d465c21066baf
inkstallgames <inkstallsushant@gmail.com>
2025-07-18 09:52:20 +0530

    Update Material of all building

--- Assets/_Scripts/Scene/LoadBuildingScene.cs
+++ Assets/_Scripts/Scene/LoadBuildingScene.cs
@@ -3,16 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class LoadBuildingScene : MonoBehaviour
 {
-     private void OnTriggerEnter(Collider other)
-     {
-          if (other.tag == "Player")
-          {
-               Invoke("LoadNextScene", 0.2f);
-          }
-     }
+    [Tooltip("The build index of the scene to load")]
+    [SerializeField] private int targetSceneIndex;
+    
+    [Tooltip("Is this an entrance to a building (true) or an exit back to the city (false)")]
+    [SerializeField] private bool isEntrance = true;
+    
+    [Tooltip("Optional delay before loading the scene")]
+    [SerializeField] private float loadDelay = 0.2f;
+    
+    [Tooltip("The main city scene index (used when exiting buildings)")]
+    [SerializeField] private int mainCitySceneIndex = 0;
 
-     private void LoadNextScene()
-     {
-          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Invoke("LoadTargetScene", loadDelay);
+        }
+    }
+
+    private void LoadTargetScene()
+    {
+        int sceneToLoad = isEntrance ? targetSceneIndex : mainCitySceneIndex;
+        SceneManager.LoadScene(sceneToLoad);
+    }
 }

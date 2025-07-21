df2e856c3c4e720618f27900f3df118a71243314
inkstallgames <inkstallsushant@gmail.com>
2025-07-21 17:52:34 +0530

    Update KeyManager.cs

--- Assets/_Scripts/KeySystem/KeyManager/KeyManager.cs
+++ Assets/_Scripts/KeySystem/KeyManager/KeyManager.cs
@@ -8,7 +8,7 @@ public class KeyManager : MonoBehaviour
     public static KeyManager Instance;
 
     [Header("Key Settings")]
-    [SerializeField] private int keysCount = 0;
+    [SerializeField] private int keysCount = 4;
     [SerializeField] private TextMeshProUGUI keyText;
     [SerializeField] private string apiUrl = "https://your-api.com/keys/player123";
 
@@ -19,6 +19,7 @@ public class KeyManager : MonoBehaviour
 
     private void Start()
     {
+        keyText.text = $"Keys: {keysCount}";
         StartCoroutine(FetchDBKeyCount());
     }
 

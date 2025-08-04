436a4db45ec85dfbcf3c3106858c81af369b0dc8
inkstallgames <inkstallsushant@gmail.com>
2025-08-04 14:18:33 +0530

    Gun animations added

--- Assets/_Scripts/Gun/Gun.cs
+++ Assets/_Scripts/Gun/Gun.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public Animator gunAnimator;
+    public AudioSource audioSource;
+    public AudioClip fireSound;
+    public bool isFiring;
 
     public void Fire()
     {
-        gunAnimator.SetTrigger("Fire");
+        if(isFiring)
+        {
+            
+            gunAnimator.SetTrigger("isFiring true");
+            audioSource.PlayOneShot(fireSound);
+        }
+        else
+        {
+            gunAnimator.SetTrigger("isFiring false");
+        }
     }
     
 }

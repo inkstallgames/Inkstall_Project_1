945b3fa5483d07121129ba3af2749eed0472c79f
Sushant Mohite <321sushantm321@gmail.com>
2025-08-20 17:38:51 +0530

    Update

--- Assets/_Scripts/Alien/Alien.cs
+++ Assets/_Scripts/Alien/Alien.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Alien : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
         StartCoroutine(DestroyAlien());
     }

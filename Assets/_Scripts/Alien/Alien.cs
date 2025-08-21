91c3ffecf431ec1997ceec90fed000dce60035ee
Sushant Mohite <321sushantm321@gmail.com>
2025-08-21 05:37:13 +0530

    Update

--- Assets/_Scripts/Alien/Alien.cs
+++ Assets/_Scripts/Alien/Alien.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem dissappearEffect;
+    [SerializeField] private ParticleSystem disappearEffectPrefab;
     [SerializeField] private AudioClip dissappearSound;
-    
+
+    AudioSource audioSource;
+
     // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     void OnEnable()
     {
         StartCoroutine(DestroyAlien());
@@ -16,6 +23,10 @@ public class Alien : MonoBehaviour
     public IEnumerator DestroyAlien()
     {
         yield return new WaitForSeconds(3f);
+        ParticleSystem effect = Instantiate(disappearEffectPrefab, transform.position, Quaternion.identity);
+        effect.Play();
+        audioSource.PlayOneShot(dissappearSound);
+        yield return new WaitForSeconds(0.1f);
         Destroy(gameObject);
     }   
 }

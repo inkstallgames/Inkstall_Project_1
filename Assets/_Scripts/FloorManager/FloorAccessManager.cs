8aa3695505b6be5a866287103ceec28391fccd3e
Chinmay20011 <inkstallsushant@gmail.com>
2025-11-04 14:12:20 +0530

    Update FloorAccessManager.cs

--- Assets/_Scripts/FloorManager/FloorAccessManager.cs
+++ Assets/_Scripts/FloorManager/FloorAccessManager.cs
@@ -63,7 +63,7 @@ public class FloorAccessManager : MonoBehaviour
         // Check each floor rule
         for (int i = 0; i < floorCondition.Count; i++)
         {
-            var condition = floorCondition[i];
+            var = floorCondition[i];
             Debug.Log($"[FloorAccessManager] Checking rule {i}: {condition.floorName} - {condition.requiredDoorIDs.Count} doors");
             CheckFloorRule(condition);
         }

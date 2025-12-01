8e434351be09f9f1224ad71bd4f540b5990e385b
inkstallsushant <inkstallsushant@gmail.com>
2025-12-01 16:58:49 +0530

    update

--- Assets/_Scripts/Scene/LoadBuildingScene.cs
+++ Assets/_Scripts/Scene/LoadBuildingScene.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class LoadBuildingScene : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class LoadBuildingScene : MonoBehaviour
     [SerializeField] private AudioClip portalEnterSound;
 
     private AudioSource audioSource;
+    public Animator animator;
 
     private void Start()
     {
@@ -44,7 +46,14 @@ public class LoadBuildingScene : MonoBehaviour
 
     private void LoadNextScene()
     {
+        StartCoroutine(TriggerTransition());
+    }
+
+    IEnumerator TriggerTransition()
+    {
+        animator.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
         int sceneToLoad = isEntrance ? targetSceneIndex : mainCitySceneIndex;
         SceneManager.LoadScene(sceneToLoad);
-    }
+    }   
 }
--- Assets/_Scripts/Transition/Transition.cs
+++ Assets/_Scripts/Transition/Transition.cs
@@ -10,18 +10,7 @@ public class Transition : MonoBehaviour
     public Animator transition;
 
 
-    public void LoadLevel()
-    {
-        StartCoroutine(TriggerTransition(SceneManager.GetActiveScene().buildIndex + 1));
-    }
-
-
-    IEnumerator TriggerTransition(int levelIndex)
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene(levelIndex);
-    }   
+    
 }
 
 

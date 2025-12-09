180d838971717171624798379835923c9ce4af78
yashinskstall-beep <yashinskstall@gmail.com>
2025-12-09 15:12:21 +0530

    emission and rigidbody

--- Assets/_Scripts/Alien/AlienPropBehaviour.cs
+++ Assets/_Scripts/Alien/AlienPropBehaviour.cs
@@ -17,8 +17,8 @@ public class AlienPropBehaviour : MonoBehaviour
     [SerializeField] private bool useEmissionGlow = true;
     [SerializeField] private Color emissionBaseColor = Color.black;   // default "off" color
     [SerializeField] private Color emissionGlowColor = Color.cyan;    // glow color
-    [SerializeField] private float emissionMaxIntensity = 1.8f;       // how strong the glow is
-    [SerializeField] private float emissionPulseDuration = 0.8f;      // seconds for a pulse
+    [SerializeField] private float emissionMaxIntensity = 1f;       // how strong the glow is
+    [SerializeField] private float emissionPulseDuration = 0.3f;      // seconds for a pulse
 
     private Renderer propRenderer;
     private MaterialPropertyBlock mpb;
